69ae4ccaaa8424536de5787cc3c89d7ad6404e73
[Rafay123Anwar] <[muhammadrafayy0@gmail.com]>
2025-04-13 18:25:09 +0500

    first commit

--- quizproject/settings.py
+++ quizproject/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i+1n*nyivz*jswu5*8h2dokr^^@ufuhmj3w%!sm&qs6c+=yczu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['.vercel.app','localhost','127.0.0.1','.now.sh']
