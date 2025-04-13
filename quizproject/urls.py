816bfa2389ef1247ac05d68e9d930debce5f4728
[Rafay123Anwar] <[muhammadrafayy0@gmail.com]>
2025-04-13 18:53:45 +0500

    first commit

--- quizproject/urls.py
+++ quizproject/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"message": "Quiz backend is running!"})
+
 
 urlpatterns = [
+    path('', health_check),
     path('admin/', admin.site.urls),
     path('api/', include('quiz.urls')),  # Ensure there's a slash after 'api'
 ]
